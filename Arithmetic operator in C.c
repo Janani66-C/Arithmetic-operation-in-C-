d4d909b23b6147a045f0b5eb70fc69739d9c7518
Janani K <janani66dj@gmail.com>
2025-08-15 19:40:28 +0530

    Add files via upload

---
+++ Arithmetic operator in C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int sum=a+b;
+	int sub=a-b;
+	int div=a/b;
+	int multiple=a*b;
+	printf("%d\n",sum);
+	printf("%d\n",sub);
+	printf("%d\n",div);
+	printf("%d\n",multiple);
+	return 0;
+	
+}
+
+
